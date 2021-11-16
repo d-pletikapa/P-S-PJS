e854a337d0a7b470cd99017c917a52c5b8fdd445
Dennys <keta548@vivaldi.net>
2021-11-16 22:26:48 +0300

    Готово!

--- js/index.js
+++ js/index.js
@@ -92,8 +92,8 @@ const enabledScroll = () => {
 // pageOverlayModal.classList.remove('page__overlay_modal_open');
 //header__contacts_open
 
-{
-	// Lesson-3 Галлерея
+{ // Lesson-3 Галлерея
+
 	const portfolioList = document.querySelector('.portfolio__list');
 	const pageOverlay = document.createElement('div');
 	pageOverlay.classList.add('page__overlay');
@@ -142,4 +142,84 @@ const enabledScroll = () => {
 	});
 
 
+}
+
+{ // Создание карточек на основе данных из JSON
+
+	const COUNT_CARD = 2;
+	const portfolioList = document.querySelector('.portfolio__list');
+	const portfolioAdd = document.querySelector('.portfolio__add');
+
+	//console.dir('portfolioAdd:', portfolioAdd);
+
+
+	const getData = () => fetch('db.json')
+		.then((response) => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw `Что-то пошло не так, попробуйте позже, ошибка: ${response.status}`;
+			}
+		}).catch(error => console.error(error));
+
+	//16:34
+	const createStore = async () => {
+		const data = await getData();
+		return {
+			data,
+			counter: 0,
+			count: COUNT_CARD,
+			get length() {
+				return this.data.length;
+			},
+			get cardData() {
+				const renderData = this.data.slice(this.counter, this.counter + this.count);
+				this.counter += renderData.length;
+				return renderData;
+			}
+		};
+	};
+
+	const renderCard = data => {
+		const cards = data.map(({ preview, year, type, client, image }) => {
+
+			const li = document.createElement('li');
+			li.classList.add('portfolio__item');
+			li.innerHTML = `
+						<article class="card" tabindex="0" role="button" aria-label="открыть макет"
+							data-full-image="${image}">
+							<picture class="card__picture">
+								<source srcset="${preview}.avif" type="image/avif">
+								<source srcset="${preview}.webp" type="image/webp">
+								<img src="${preview}.jpg" alt="превью iphone" width="166" height="103">
+							</picture>
+
+							<p class="card__data">
+								<span class="card__client">Клиент: ${client}</span>
+								<time class="card__date" datetime="${year}">год: ${year}</time>
+							</p>
+
+							<h3 class="card__title">${type}</h3>
+						</article>
+			`;
+			return li;
+		});
+		portfolioList.append(...cards);
+	};
+
+
+	const initPortfolio = async () => {
+		const store = await createStore();
+		renderCard(store.cardData);
+		portfolioAdd.addEventListener('click', () => {
+			renderCard(store.cardData);
+			//console.log('', store.cardData);
+			//console.log('', store.length, store.counter);
+			if (store.length === store.counter) {
+				portfolioAdd.remove();
+			}
+
+		});
+	};
+	initPortfolio();
 }
